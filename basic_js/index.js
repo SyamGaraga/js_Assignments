22e17046fd03649aee829be9f6068bea5045ec63
SyamGaraga <syamgaragaabc@gmail.com>
2025-04-21 23:05:39 +0530

    updated

---
+++ basic_js/index.js
@@ -0,0 +1,24 @@
+// first method
+console.log("Hello, World!");
+
+// second method
+function sayHello() {
+    console.log("Hello, World!");
+}
+
+sayHello();
+
+
+
+// third method
+class Greeter {
+    sayHello() {
+        console.log("Hello, World!");
+    }
+}
+
+const greeter = new Greeter();
+greeter.sayHello();
+
+
+
